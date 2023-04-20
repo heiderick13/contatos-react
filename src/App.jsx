46e82ea8a80a9c2448eebe934e5f5f745feb1710
Marcelo Oliveira <marceloheiderick@outlook.com>
2023-04-20 18:57:23 -0300

    :sparkles: page header

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
+import "./App.css";
+
+import { useState, useRef, useEffect } from "react";
+
+import { MdOutlineSearch } from "react-icons/md";
+import { MdAdd } from "react-icons/md";
+import { MdDeleteOutline } from "react-icons/md";
+
 function App() {
   return (
     <>
-      <div>App</div>
+      <header>
+        <div className="head flex">
+          <div className="head-title">
+            <h1>Meus contatos</h1>
+          </div>
+          <div className="head-icons flex">
+            <MdOutlineSearch />
+            <MdAdd />
+            <MdDeleteOutline />
+          </div>
+        </div>
+      </header>
     </>
   );
 }
