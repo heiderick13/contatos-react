a23696a58c567d7906acadad9500d0f6b18a7148
Marcelo Oliveira <marceloheiderick@outlook.com>
2023-04-25 17:52:24 -0300

    :lipstick: update card

--- src/components/ContactCard/ContactCard.jsx
+++ src/components/ContactCard/ContactCard.jsx
@@ -3,7 +3,7 @@ import "./ContactCard.css";
 function ContactCard({ name, phoneNumber }) {
   return (
     <div className="contact flex">
-      <div className="fake-img">m</div>
+      <div className="fake-img">{name.split("")[0]}</div>
       <div className="info">
         <p className="name">{name}</p>
         <p className="number">{phoneNumber}</p>
